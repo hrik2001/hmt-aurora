de4918bb4530172b0e7b995fc7d79462b5b64469
rik61072@gmail.com <rik61072@gmail.com>
2022-10-25 23:46:05 +0530

    initial commit with testnet details

---
+++ hmt-escrow/migrations/2_deploy_contracts.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const path = require('path');
+
+const HMToken = artifacts.require('./HMToken.sol');
+const EscrowFactory = artifacts.require("EscrowFactory");
+const KVStore = artifacts.require("KVStore");
+
+const ADDRESS_OUTPUT_FILENAME = '/deployed-hmtoken/hmt.address.json';
+
+module.exports = (deployer) => {
+  deployer.then(async () => {
+    await deployer.deploy(EscrowFactory, '0x9406d5c635AD22b0d76c75E52De57A2177919ca3');
+    await deployer.deploy(KVStore);
+  })
+};
